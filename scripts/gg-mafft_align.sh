b8c277ad1a52e31237e2eb4f389f7302a874b29a
Nathan Dunn <ndunn@me.com>
2016-10-26 17:42:30 +0000

    small fixe to make a little faster

--- scripts/gg-mafft_align.sh
+++ scripts/gg-mafft_align.sh
@@ -62,7 +62,7 @@ function copyEmpty(){
 
 	  #echo "non-alignment BAM input $outputfile and output $bamoutput"
 	  #echo "./scripts/fas2bam.pl --input $outputfile --output $bamoutput --ref 'ref' --bamheader './scripts/windowbam.header.txt'"
-	  ./scripts/fas2bam.pl --input $outputfile --output $bamoutput --ref "ref" --bamheader "./scripts/windowbam.header.txt"
+	  ./scripts/fas2bam.pl --input $outputfile --output $bamoutput --ref "ref" --bamheader "./scripts/windowbam.header.txt" &
 	done
 	echo "Copied $i files."
 }
@@ -96,7 +96,7 @@ function align(){
 
   #echo "BAM input $outputfile and output $bamoutput"
 
-  ./scripts/fas2bam.pl --input $outputfile --output $bamoutput --ref "ref" --bamheader "./config/windowbam.header.txt"
+  ./scripts/fas2bam.pl --input $outputfile --output $bamoutput --ref "ref" --bamheader "./config/windowbam.header.txt" &
 
 }
 
