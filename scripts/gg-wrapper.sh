efd1c5c7f44ed3f767f605953cd85aac85136084
jcoliver <jcoliver@email.arizona.edu>
2016-10-26 06:39:28 -0700

    Update wrapper script with actual pl/py script names

--- scripts/gg-wrapper.sh
+++ scripts/gg-wrapper.sh
@@ -49,22 +49,21 @@ LOCALBAMFILE="localbam"
 
 ### Step 1: Globally align all contigs
 # + Call Local to Global Alignment
-./gg-01.1-local.sh -g $REFFASTA -c $CONTIGDIR -b $LOCALBAMFILE
+# TODO: Update with a real Call
+# PSEUDOCALL
+scripts/NWAM_01.py -g $REFFASTA -c $CONTIGDIR -b $LOCALBAMFILE
+#./gg-01.1-local.sh -g $REFFASTA -c $CONTIGDIR -b $LOCALBAMFILE
 
 ### Step 2: Divide and conquer multiple sequence alignment
 # Call BAM2MAFFT for window identification, extraction, and FASTA conversion
 BAM2MAFFT.pl --referenceFasta $REFFASTA --BAM $LOCALBAMFILE
 
 # MAFFT alignment
-# TODO: check the arguments for this path; it might just take two arguments:
-# The output destination for FASTA files, and input directory for FASTA
+# Many FASTA -> Many BAM
 FASTADIR=""
 BAMDIR=""
-./align_mafft.sh -i $FASTADIR -o $BAMDIR
-
-# Many FASTA -> Many BAM
-# TODO: The perl script fas2bam.pl needs to be called for each window (ideally
-# in parallel; could just be added to updated version of align_mafft.sh)
+# TODO: Will ultimately also pass number of processors & number of threads
+./gg-align_mafft.sh -i $FASTADIR -o $BAMDIR
 
 # Many BAM -> single BAM
 FINALBAM="uberbam.bam"
@@ -79,9 +78,10 @@ VCFFILE="myvcf.vcf"
 # BAM to VCF
 # TODO: Insert call to Andrew's code here
 # PSEUDOCALL:
-bam2vcf.pl -b $FINALBAM -o $VCFFILE
+BAM2VCF.pl -b $FINALBAM -o $VCFFILE
 
 # Call vg to convert VCF to vg or gfa format
 VGOUTFILE="vgoutput.vg"
 # TODO: What is the small/x.fa argument? a reference genome?
+# PSEUDOCALL:
 vg construct -r small/x.fa -v $VCFFILE > $VGOUTFILE
