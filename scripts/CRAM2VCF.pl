8b133c21dbf4871a37764e20bb5071a7ed5fff54
evanbiederstedt <evanbiederstedt@users.noreply.github.com>
2018-07-02 11:03:42 -0400

    Update CRAM2VCF.pl

--- scripts/CRAM2VCF.pl
+++ scripts/CRAM2VCF.pl
@@ -21,9 +21,9 @@ $| = 1;
 ##
 ## Example command:
 ## ./CRAM2VCF.pl --CRAM /intermediate_files/combined.cram
-##     --referenceFasta GRCh38_full_plus_hs38d1_analysis_set_minus_alts.fa
-##     --output VCF/graph.vcf
-##     --contigLengths /intermediate_files/postGlobalAlignment_readLengths
+##               --referenceFasta GRCh38_full_plus_hs38d1_analysis_set_minus_alts.fa
+##               --output VCF/graph.vcf
+##               --contigLengths /intermediate_files/postGlobalAlignment_readLengths
 
 
 my $CRAM;
