b9ae685d36abe8ac017296bb697bc07accc67bfc
torhou <torsten.houwaart@med.uni-duesseldorf.de>
2020-01-13 12:33:45 +0100

    Parsing the biopython way is better

--- scripts/split_identify_fasta.py
+++ scripts/split_identify_fasta.py
@@ -11,35 +11,44 @@ parser.add_argument("outputfile", help="Output with split fasta records")
 parser.add_argument("--Ns", type = int, default = 5000, help = "Number of Ns to split by [5000]")
 
 args = parser.parse_args()
+def read_fasta(fp):
+    name, seq = None, []
+    for line in fp:
+        line = line.rstrip()
+        if line.startswith(">"):
+            if name: yield (name, ''.join(seq))
+            name, seq = line, []
+        else:
+            seq.append(line)
+    if name: yield (name, ''.join(seq))
+     
 
 with open(args.outputfile, "w+") as outf:
     with open(args.inputfile) as f:
-        for linenr, line in enumerate(f):
-            if linenr % 2 == 0:
-                rid = line.rstrip().lstrip(">")
-            else:
-                if line[0] not in {'A', 'G', 'C', 'T', 'N'}:
-                    print("Error found in input file. Make sure the input is in correct fasta format.")
-                    sys.exit()
-                print(rid + "... ", end = "")
+        for fid, seqt in read_fasta(f):
+            seq = seqt.upper()
+            if seq[0] not in {'A', 'G', 'C', 'T', 'N'}:
+                print("Error found in input file. Make sure the input is in correct fasta format.")
+                sys.exit()
+            print(fid + "... ", end = "")
 
-                s = line.rstrip().upper().lstrip("N").rstrip("N")
-                matches = re.finditer("N{" + str(args.Ns) + ",}", s)
-                start = 0
-                partnr = 1
-                for m in matches:
-                    nstart, nstop = m.span()
-                    #print(s[m.span()[1]])
-                    part = s[start:nstart]
+            s = seq.rstrip().upper().lstrip("N").rstrip("N")
+            matches = re.finditer("N{" + str(args.Ns) + ",}", s)
+            start = 0
+            partnr = 1
+            for m in matches:
+                nstart, nstop = m.span()
+                #print(s[m.span()[1]])
+                part = s[start:nstart]
 
-                    outf.write(">" + args.prefix + "_" + rid + "_" + str(partnr) + "\n")
-                    outf.write(part + "\n")
-
-                    partnr += 1
-                    start = nstop
-                part = s[start:]
-                outf.write(">" + args.prefix + "_" + rid + "_" + str(partnr) + "\n")
+                outf.write(">" + args.prefix + "_" + fid + "_" + str(partnr) + "\n")
                 outf.write(part + "\n")
-                
-                print("found " + str(partnr) + " parts")
+
+                partnr += 1
+                start = nstop
+            part = s[start:]
+            outf.write(">" + args.prefix + "_" + fid + "_" + str(partnr) + "\n")
+            outf.write(part + "\n")
+            
+            print("found " + str(partnr) + " parts")
 
